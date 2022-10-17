7664ffec5379646b061c26df417aa6c11832b92c
clarenz23 <112999859+clarenz23@users.noreply.github.com>
2022-10-17 18:58:01 +0800

    Update login.js

--- scripts/login.js
+++ scripts/login.js
@@ -1,44 +1,6 @@
 let attempt = 3; // set available attempts to 3
 
-const csvUserFile = document.getElementById("csvUserInput");
-const myForm = document.getElementById("myForm");
 
-function csvToArray(str, delimiter = ",") {
-    /**
-     * slice from start of text to the first \n index
-     * use split to create an array from string by delimiter
-     */
-    const headers = str.slice(0, str.indexOf("\n").split(delimiter));
-
-    /**
-     * slice from \n index + 1 to the end of the text
-     * use split to create an array of each csv value row
-     */
-    const rows = str.slice(str.indexOf("\n") + 1).split("\n");
-
-    // return the array
-    return rows.map(function (row) {
-        const values = row.split(delimiter);
-        return headers.reduce(function (object, header, index) {
-            object[header] = values[index];
-            return object;
-        }, {});
-    });
-}
-
-myForm.addEventListener("submit", function (e) {
-    e.preventDefault();
-    const input = csvUserFile.files[0];
-    const reader = new FileReader();
-
-    reader.onload = function (e) {
-        const text = e.target.result;
-        const data = csvToArray(text);
-        document.write(JSON.stringify(data));
-    };
-
-    reader.readAsText(input);
-});
 
 /**
  * clears username and password input field
