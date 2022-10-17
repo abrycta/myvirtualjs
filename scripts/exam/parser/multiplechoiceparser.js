6c93187b9f3ed80c4c8ad41b59062940b5c148cd
cccjjjjjj <113432966+cccjjjjjj@users.noreply.github.com>
2022-10-17 23:55:32 +0800

    Update multiplechoiceparser.js

--- scripts/exam/parser/multiplechoiceparser.js
+++ scripts/exam/parser/multiplechoiceparser.js
@@ -1,7 +1,5 @@
 const fs = require('fs');
 fs.readFile('res/multipleChoice.json', 'utf8', (err, data) => {
     if(err) console.error(err);
-    const identify = JSON.parse(data);
-
-    console.log(identify.value);
+    const multiChoice = JSON.parse(data);
 });
