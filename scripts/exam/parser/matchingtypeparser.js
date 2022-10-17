24e996ae792b325c04ff71839c83799e6816731f
cccjjjjjj <113432966+cccjjjjjj@users.noreply.github.com>
2022-10-17 23:54:51 +0800

    Update matchingtypeparser.js

--- scripts/exam/parser/matchingtypeparser.js
+++ scripts/exam/parser/matchingtypeparser.js
@@ -2,6 +2,4 @@ const fs = require('fs');
 fs.readFile('res/matchingType.json', 'utf8', (err, data) => {
     if(err) console.error(err);
     const identify = JSON.parse(data);
-
-    console.log(identify.value);
 });
