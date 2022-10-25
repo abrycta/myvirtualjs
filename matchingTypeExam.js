78639515c94ee31a1ad60b4e4351f2ff4391ead1
cccjjjjjj <113432966+cccjjjjjj@users.noreply.github.com>
2022-10-26 01:01:20 +0800

    pls help :( dropzone :(

--- matchingTypeExam.js
+++ matchingTypeExam.js
@@ -1,11 +1,52 @@
+import {multipleChoiceExamStart} from "./multipleChoiceExam.js";
+
 const matchingExam = JSON.parse(localStorage.getItem('matchingExam'))
 const body = document.body
+
+
+const item = document.querySelector('.item')
+if (item){
+    item.addEventListener('dragstart', dragStart)
+}
+function dragStart(e) {
+    e.dataTransfer.setData('text/plain', e.target.id)
+    setTimeout(() => {
+        e.target.classList.add('hide')
+    }, 0)
+}
+const boxes = document.querySelectorAll('.box')
+boxes.forEach(box => {
+    box.addEventListener('dragenter', dragEnter)
+    box.addEventListener('dragover', dragOver)
+    box.addEventListener('dragleave', dragLeave)
+    box.addEventListener('drop', drop)
+})
+function dragEnter(e) {
+    e.preventDefault();
+    e.target.classList.add('drag-over')
+}
+function dragOver(e) {
+    e.preventDefault();
+    e.target.classList.add('drag-over')
+}
+function dragLeave(e) {
+    e.target.classList.remove('drag-over')
+}
+function drop(e) {
+    e.target.classList.remove('drag-over')
+    const id = e.dataTransfer.getData('text/plain')
+    const draggable = document.getElementById(id)
+    e.target.appendChild(draggable)
+    draggable.classList.remove('hide')
+}
+
+
 const renderMatchingItem = (item, index) => {
     const htmlItem = document.createElement('div2')
 
     htmlItem.innerHTML=`
         <h3>${item.Answer}</h3>
-        <div class = "dropzone" id = "dropTarget"></div>
+        <div class = "drop-targets" id = "dropTarget"></div>
     `
     body.append(htmlItem)
 }
@@ -13,7 +54,7 @@ const renderMatchingItem = (item, index) => {
 const renderChoice = (item, index) => {
     const choiceItem = document.createElement('div1')
     choiceItem.innerHTML=`
-        <h2 class="box" draggable="true">${item.Choice}</h2>
+        <h2 class="item" id="item" draggable="true">${item.Choice}</h2>
     `
     body.append(choiceItem)
 }
@@ -27,8 +68,18 @@ function returnButton() {
     body.append(returnPage)
 }
 
+function backButton() {
+    const nextPage = document.createElement('backButton')
+    nextPage.innerText = "Back"
+    nextPage.addEventListener('click', () => {
+        document.body.innerHTML = '';
+        multipleChoiceExamStart()
+    })
+    body.append(nextPage)
+}
+
 function submitButton() {
-    const submitPage = document.createElement('SubmitButton')
+    const submitPage = document.createElement('submitButton')
     submitPage.innerText = "Submit"
     submitPage.addEventListener('click', () => {
         window.location.href = "./testproper.html"
@@ -50,6 +101,7 @@ function matchingTypeExam() {
             matchingExam['questions'].indexOf(item))
     })
     returnButton()
+    backButton()
     submitButton()
 }
 
