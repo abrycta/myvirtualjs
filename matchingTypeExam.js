bb58d25dcdfcdb88083df31a5a8da59fa3f3c4f8
Kurt <kurtmtthw@gmail.com>
2022-10-26 23:53:05 +0800

    All types of exam now correctly saving state.

--- matchingTypeExam.js
+++ matchingTypeExam.js
@@ -1,63 +1,119 @@
 import {multipleChoiceExamStart} from "./multipleChoiceExam.js";
 import {multiChoiceExam} from "./testproper.js";
-
+import {studentSession} from "./testproper.js";
 const matchingExam = JSON.parse(localStorage.getItem('matchingExam'))
 const body = document.body
 
+// class has-data
 
-const item = document.querySelector('.item')
-if (item){
-    item.addEventListener('dragstart', dragStart)
-}
+
 function dragStart(e) {
     e.dataTransfer.setData('text/plain', e.target.id)
     setTimeout(() => {
         e.target.classList.add('hide')
     }, 0)
 }
-const boxes = document.querySelectorAll('.box')
-boxes.forEach(box => {
-    box.addEventListener('dragenter', dragEnter)
-    box.addEventListener('dragover', dragOver)
-    box.addEventListener('dragleave', dragLeave)
-    box.addEventListener('drop', drop)
-})
+
+// Functions just for preventing the default mechanism of e
 function dragEnter(e) {
     e.preventDefault();
-    e.target.classList.add('drag-over')
+
 }
 function dragOver(e) {
     e.preventDefault();
-    e.target.classList.add('drag-over')
 }
 function dragLeave(e) {
     e.target.classList.remove('drag-over')
 }
+
+// Function for managing the drop event
 function drop(e) {
     e.target.classList.remove('drag-over')
+
+    let has_data =  e.target.hasChildNodes() // for checking if there is already an answer
     const id = e.dataTransfer.getData('text/plain')
-    const draggable = document.getElementById(id)
-    e.target.appendChild(draggable)
-    draggable.classList.remove('hide')
+    let draggable = document.getElementById(id)
+    // if no answer
+    if (!has_data){
+        
+        // Get the parent div and delete it
+        let parentDiv = draggable.parentNode
+        e.target.appendChild(draggable)
+        if (!parentDiv.classList.contains("drop-targets")){ // Prevent from deleting the div of the questions
+            parentDiv.remove()
+        }
+        
+
+        // Re-add Event Listener
+        draggable = document.getElementById(id)
+        draggable.addEventListener('dragstart', dragStart)
+
+        draggable.classList.remove('hide')
+    } else {
+        let currentItem = e.target
+        swap(currentItem, draggable)
+    
+    }
+    
 }
 
 
 const renderMatchingItem = (item, index) => {
-    const htmlItem = document.createElement('div2')
+
+    let previousAnswer = studentSession['matchingAnswers'].get(index)
+   
+    const htmlItem = document.createElement('div')
+    let itemsDiv = document.getElementById("itemsDiv")
 
     htmlItem.innerHTML=`
         <h3>${item.Answer}</h3>
-        <div class = "drop-targets" id = "dropTarget"></div>
+        <div class = "drop-targets" id = "dropTarget${index}"></div>
     `
-    body.append(htmlItem)
+    itemsDiv.append(htmlItem)
+    let dropTarget = document.getElementById("dropTarget"+index);
+
+    // For Re adding previous answer
+    if (previousAnswer){
+        let answer = previousAnswer[0]
+        let oldIndex = previousAnswer[1] // for making sure oldIndex was copied as used in preventing duplicate
+        if (answer != null){ // if there is a previous answer (just a safety net)
+            // Create an element by code
+            const choiceItem = document.createElement('h2')
+            choiceItem.classList.add('item');
+            choiceItem.id = oldIndex;
+            choiceItem.draggable = true;
+            choiceItem.innerHTML = answer;
+            choiceItem.addEventListener('dragstart', dragStart) // Apply event listener
+            dropTarget.append(choiceItem);
+        }
+    }
+    
+    // Add an event listener for watching the drop thingies
+    
+    dropTarget.addEventListener('dragenter', dragEnter)
+    dropTarget.addEventListener('dragover', dragOver)
+    dropTarget.addEventListener('dragleave', dragLeave)
+    dropTarget.addEventListener('drop', drop)
 }
 
+
+
 const renderChoice = (item, index) => {
-    const choiceItem = document.createElement('div1')
-    choiceItem.innerHTML=`
-        <h2 class="item" id="item" draggable="true">${item.Choice}</h2>
-    `
-    body.append(choiceItem)
+    let choiceItem = document.getElementById("item"+index); // For checking if there already duplicate (from previous answer)
+    let choicesDIv = document.getElementById("choicesDiv")
+    if (!choiceItem){ // Only run if no previous answer
+        const div = document.createElement('div');
+        choiceItem = document.createElement('h2');
+        choiceItem.classList.add('item');
+        choiceItem.id = "item"+index;
+        choiceItem.draggable = true;
+        choiceItem.innerHTML = item.Choice;
+
+        choiceItem.addEventListener('dragstart', dragStart) // reapply event listener
+        div.append(choiceItem)
+        choicesDIv.append(div)
+    }
+    
 }
 
 function returnButton() {
@@ -69,17 +125,66 @@ function returnButton() {
     body.append(returnPage)
 }
 
+
 function backButton() {
     const nextPage = document.createElement('backButton')
     nextPage.innerText = "Back"
+    /*
     nextPage.addEventListener('click', () => {
-        document.body.innerHTML = '';
-        multiChoiceExam['questions'].forEach((item) => {
-            renderMatchingItem(item,
-                multiChoiceExam['questions'].indexOf(item))
+        // save progress
+        let selectedElements = document.querySelectorAll("[name^='multiForm_']")
+        let index = 0
+        let prevItem
+        console.log(selectedElements)
+        /*
+        selectedElements.forEach((item) => {
+            try {
+                prevItem = item
+                    .querySelector("input[name ^= 'multi_']:checked")
+                    .value
+                studentSession['multiChoiceAnswers'].set(index, prevItem)
+                index++
+            }
+            catch (TypeError) {
+                console.log('jkjkjkj')
+                studentSession['multiChoiceAnswers'].set(index, null)
+                console.log(studentSession['multiChoiceAnswers'])
+                index++
+            }
         })
-        // multipleChoiceExamStart()
+        
+    }) */
+
+    nextPage.addEventListener('click', () => {
+        // Experimental Code
+        let selectedElements = document.querySelectorAll("[id^='dropTarget']")
+        let index = 0
+        console.log("Drop Targetaaa",selectedElements)
+        selectedElements.forEach((item) => {
+            try{
+                let indexV = null // for storing old index of the answer
+                let has_data =  item.hasChildNodes() // check if the question has an answer
+                let value = null
+                if (has_data){
+                    value = item.firstChild.innerHTML
+                    indexV = item.firstChild.id
+                }
+                if (value != null){
+                    studentSession['matchingAnswers'].set(index, [value, indexV])
+                }
+                
+                index++
+            }catch (TypeError){
+
+            }
+            
+        })
+        
+        document.body.innerHTML = ''
+        multipleChoiceExamStart()
     })
+
+
     body.append(nextPage)
 }
 
@@ -94,6 +199,26 @@ function submitButton() {
 
 function matchingTypeExam() {
     console.log(matchingExam.questions)
+
+    // For 2 column div currently is not responsive for mobile screens
+    const container = document.createElement('span');
+    container.style.display = "flex"
+    const itemsDiv = document.createElement('span');
+    itemsDiv.id = "itemsDiv"
+    const choicesDiv = document.createElement('span');
+    choicesDiv.id = "choicesDiv"
+    choicesDiv.style.margin = 0;
+
+    itemsDiv.style.flexGrow = 1;
+    choicesDiv.style.flexGrow = 1;
+
+    container.appendChild(itemsDiv)
+    container.appendChild(choicesDiv)
+    body.append(container)
+    
+
+
+
     // start matching type exam
 
     matchingExam['questions'].forEach((item) => {
@@ -110,4 +235,15 @@ function matchingTypeExam() {
     submitButton()
 }
 
+function swap(nodeA, nodeB) {
+    const parentA = nodeA.parentNode;
+    const siblingA = nodeA.nextSibling === nodeB ? nodeA : nodeA.nextSibling;
+
+    // Move `nodeA` to before the `nodeB`
+    nodeB.parentNode.insertBefore(nodeA, nodeB);
+
+    // Move `nodeB` to before the sibling of `nodeA`
+    parentA.insertBefore(nodeB, siblingA);
+}
 export {matchingTypeExam}
+
--- multipleChoiceExam.js
+++ multipleChoiceExam.js
@@ -11,14 +11,21 @@ const renderMultipleChoiceItem = (item, index) => {
     htmlItem.innerHTML = `
     <h2 class="questionText">${item.question}</h2>
     <form name="multiForm_${index}">
-            <input type="radio" name="multi_${index}" id ="${item.a}" value="${item.a}"/>
-            <label for = "${item.a}">${item.a}</label>
-            <input type="radio" name="multi_${index}" id ="${item.b}" value="${item.b}"/>
-            <label for = "${item.b}">${item.b}</label>
-            <input type="radio" name="multi_${index}" id ="${item.c}" value="${item.c}"/>
-            <label for = "${item.c}">${item.c}</label>
-            <input type="radio" name="multi_${index}" id ="${item.d}" value="${item.d}"/>
-            <label for = "${item.d}">${item.d}</label>
+        <ul>
+            
+            <li><input type="radio" name="multi_${index}" id ="${item.a + index}" value="${item.a}"/>
+            <label for = "${item.a + index}">${item.a}</label></li>
+            
+            <li><input type="radio" name="multi_${index}" id ="${item.b + index }" value="${item.b}"/>
+            <label for = "${item.b + index}">${item.b}</label></li>
+            
+            <li><input type="radio" name="multi_${index}" id ="${item.c + index}" value="${item.c}"/>
+            <label for = "${item.c + index}">${item.c}</label></li>
+            
+            <li><input type="radio" name="multi_${index}" id ="${item.d + index}" value="${item.d}"/>
+            <label for = "${item.d + index}">${item.d}</label></li>
+            
+            </ul>
     </form>
     `
     // previously stored item, if any
@@ -29,16 +36,17 @@ const renderMultipleChoiceItem = (item, index) => {
         // [name^='multiForm_']
             console.log(htmlItem
                 .lastElementChild
-                .querySelector(`[value =${previousAnswer}]`)
-        )
+                .querySelector(`[value ="${previousAnswer}"]`))
             htmlItem
                 .lastElementChild
-                .querySelector(`#${previousAnswer}`)
+                .querySelector(`[value ="${previousAnswer}"]`)
                 .checked = true
+            console.log(`[value =${previousAnswer}]`)
         } catch (TypeError) {
-            console.log(htmlItem.lastElementChild)
+            
+            console.log(htmlItem, TypeError, htmlItem.lastElementChild,`[value ="${previousAnswer}"]`)
+            //console.log(htmlItem.lastElementChild)
         }
-
 
     body.append(htmlItem)
 }
