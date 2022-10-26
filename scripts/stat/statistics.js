80722e21dfd965305fd7997a32ec1929afe8d3c2
2212093 <2212093@slu.edu.ph>
2022-10-26 15:26:21 +0800

    modified highest & lowest

--- scripts/stat/statistics.js
+++ scripts/stat/statistics.js
@@ -1,3 +1,4 @@
+const body = document.body
 function averageScore(){
     let average = "";
     let avg = sum / scoreArray.length;
@@ -11,9 +12,16 @@ function highestScoring() {
     let highScore = Math.max.apply(Math, scores);
     for (let i in scores) {
         if (scores[i] > highScore) highScore = scores[i];
-        bestStudent += "Highest Scoring Student = " + names[i] + "\n" + "Highest Score = " + highScore;
+        bestStudent += names[i] + highScore;
     }
-    $("results").value = bestStudent;
+    const div = document.createElement('div')
+    div.innerHTML = `
+        <h2>Highest Scoring Student</h2>
+        <h3>${bestStudent}<h3>
+        <h2>Highest Score</h2>
+        <h3>score<h3>`
+    body.append(div)
+    // $("results").value = bestStudent;
     }
 
 function lowestScoring() {
@@ -21,9 +29,16 @@ function lowestScoring() {
     let lowestScore = Math.min.apply(Math, scores);
     for (let i in scores) {
         if (scores[i] < lowestScore) lowestStudent = scores[i];
-        lowestStudent += "Lowest Scoring Student = " + names[i] + "\n" + "Lowest Score = " + lowestScore;
+        lowestStudent += names[i] + lowestScore;
     }
-    $("results").value = lowestStudent;
+    const div = document.createElement('div')
+    div.innerHTML = `
+        <h2>Lowest Scoring Student</h2>
+        <h3>${lowestStudent}<h3>
+        <h2>Lowest Score</h2>
+        <h3>score<h3>`
+    body.append(div)
+    // $("results").value = lowestStudent;
 }
 
 function mostAnsweredQ() {
